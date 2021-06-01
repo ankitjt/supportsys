c268343ecedc5d75fb3f028a502212f8edfb0f3a
ankitjt <ankitjeetthakur@gmail.com>
2021-06-01 13:58:35 +0530

    default structure added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className='App'>
-            <h1>Customer Support System.</h1>
+            <p>Customer Support System.</p>
         </div>
     );
 }
