127efafa779fd18cec9c88c2433aeb9722a92d6b
ankitjt <ankitjeetthakur@gmail.com>
2021-06-09 10:43:04 +0530

    routes and components added

---
+++ frontend/src/components/member/routes/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./MemberRoutes.css";
+const Home = () => {
+    return (
+        <div className='module-wrapper'>
+            <h1>Member Homepage.</h1>
+        </div>
+    );
+};
+
+export default Home;
