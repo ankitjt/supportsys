ffdcb02a3d25c3e2085c2b7f6dafb88652902362
ankitjt <ankitjeetthakur@gmail.com>
2021-06-30 22:18:34 +0530

    updated component details

--- frontend/src/components/Users/member/routes/Root.js
+++ frontend/src/components/Users/member/routes/Root.js
@@ -4,6 +4,7 @@ const Root = () => {
   return (
     <div className='module-wrapper'>
       <h1>Click <i>Home</i> to record Issues.</h1>
+      <p>More updates coming soon.</p>
     </div>
   );
 }
